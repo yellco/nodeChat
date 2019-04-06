2514ce8f8240e6861f92780f23a1591eb1e7a6ea
Dmitriy <dazheltyy@gmail.com>
2019-04-06 23:21:22 +0300

    Add styles

--- index.js
+++ index.js
@@ -3,33 +3,6 @@ var app = express();
 var server = require("http").createServer(app).listen(7777);
 var io = require("socket.io").listen(server);
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database : 'test-chat',
-//   port: '7777'
-// });
-
-// connection.connect();
-
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//     if (error) throw error;
-//     console.log('The solution is: ', results[0].solution);
-//   });
-
-// // connection.connect(function(err) {
-// //     if (err) {
-// //       console.error('error connecting: ' + err.stack);
-// //       return;
-// //     }
-  
-// //     console.log('connected as id ' + connection.threadId);
-// // });
- 
-// connection.end();
-
 app.get('/', (req,resp) => {
     resp.sendFile(__dirname + "/index.html");
 });
@@ -37,19 +10,31 @@ app.get('/style.css', (req,resp) => {
     resp.sendFile(__dirname + "/style.css");
 });
 
-users = [];
+var users = {
+    count: 0
+};
 connections = [];
 
 io.sockets.on('connection', function(socket){
     console.log("Успешное соединение");
+    
     connections.push(socket);
+    var address = socket.handshake.address;
+
+    users.count++;
+    io.sockets.emit('new_connect', [users.count,address]);
     
     socket.on('disconnect', function(data){
         console.log("Отключено");
         connections.splice(connections.indexOf(socket), 1);
+        users.count = users.count-1;
     });
 
     socket.on('send mess', function(data){
-        io.sockets.emit('add mess', {msg: data});
+        io.sockets.emit('add mess', {
+            mess: data.mess, 
+            name: data.name,
+            time: data.time
+        });
     });
 });
