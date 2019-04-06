38052912ff5e2a2412698478f6cfc483ecc6f5d1
demoniik111 <dazheltyy@gmail.com>
2019-04-06 19:55:39 +0300

    Add working sender and viewer

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ server.listen(7777);
 app.get('/', (req,resp) => {
     resp.sendFile(__dirname + "/index.html");
 });
+
+users = [];
+connections = [];
+
+io.sockets.on('connection', function(socket){
+    console.log("Успешное соединение");
+    connections.push(socket);
+    
+    socket.on('disconnect', function(data){
+        console.log("Отключено");
+        connections.splice(connections.indexOf(socket), 1);
+    });
+
+    socket.on('send mess', function(data){
+        io.sockets.emit('add mess', {msg: data});
+    });
+});
